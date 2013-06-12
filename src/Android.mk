d98607971586b2f54f000a3311d852848a701b8c
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2013-06-11 23:42:10 -0500

    Android.mk: update for 73
    Change-Id: I1029b20a3bf89fadfd607916e4ec2692f4d5c54e

--- src/Android.mk
+++ src/Android.mk
@@ -5,13 +5,55 @@ LOCAL_PATH:= $(call my-dir)
 # ========================================================
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES:= \
-	buffer.c charset.c diff.c digraph.c edit.c eval.c ex_cmds.c ex_cmds2.c \
-	ex_docmd.c ex_eval.c ex_getln.c fileio.c fold.c getchar.c hardcopy.c \
-	hashtab.c if_cscope.c if_xcmdsrv.c main.c mark.c memfile.c memline.c \
-	menu.c message.c misc1.c misc2.c move.c mbyte.c normal.c ops.c option.c \
-	os_unix.c auto/pathdef.c popupmnu.c quickfix.c regexp.c screen.c search.c \
-	spell.c syntax.c tag.c term.c ui.c undo.c version.c window.c \
+LOCAL_SRC_FILES := \
+	blowfish.c \
+	buffer.c \
+	charset.c \
+	diff.c \
+	digraph.c \
+	edit.c \
+	eval.c \
+	ex_cmds.c \
+	ex_cmds2.c \
+	ex_docmd.c \
+	ex_eval.c \
+	ex_getln.c \
+	fileio.c \
+	fold.c \
+	getchar.c \
+	hardcopy.c \
+	hashtab.c \
+	if_cscope.c \
+	if_xcmdsrv.c \
+	main.c \
+	mark.c \
+	memfile.c \
+	memline.c \
+	menu.c \
+	message.c \
+	misc1.c \
+	misc2.c \
+	move.c \
+	mbyte.c \
+	normal.c \
+	ops.c \
+	option.c \
+	os_unix.c \
+	auto/pathdef.c \
+	popupmnu.c \
+	quickfix.c \
+	regexp.c \
+	screen.c \
+	search.c \
+	sha256.c \
+	spell.c \
+	syntax.c \
+	tag.c \
+	term.c \
+	ui.c \
+	undo.c \
+	version.c \
+	window.c
 
 LOCAL_C_INCLUDES += \
 	$(LOCAL_PATH) \

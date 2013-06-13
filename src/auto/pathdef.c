b3345be9b6b22c60c95b3d721550fc82d5517217
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2013-06-12 22:26:48 -0500

    run cofigure for 73
    vim_cv_toupper_broken=no vim_cv_terminfo=yes vim_cv_tgent=zero vim_cv_stat_ignores_slash=no vim_cv_tty_group=system vim_cv_tty_mode=0666 vim_cv_getcwd_broken=no vim_cv_memmove_handles_overlap=yes CPPFLAGS="-I/opt/android/ev/jb/prebuilts/ndk/current/platforms/android-9/arch-arm/usr/include/" CFLAGS="-nostdlib" LDFLAGS="-Wl,-rpath-link=/opt/android/ev/jb/prebuilts/ndk/current/platforms/android-9/arch-arm/usr/lib/ -L/opt/android/ev/jb/prebuilts/ndk/current/platforms/android-9/arch-arm/usr/lib/" LIBS="-lc" ./configure --prefix=/system --target=arm-linux-androdeabi --host=arm-linux-androideabi --disable-selinux --with-tlib=ncurses --datadir=/system/usr/share
    
    with libncurses copied to ndk/*/lib
    
    Change-Id: I1431a95047d1895b74aad115684cf4503e177734

---
+++ src/auto/pathdef.c
@@ -0,0 +1,10 @@
+/* pathdef.c */
+/* This file is automatically created by Makefile
+ * DO NOT EDIT!  Change Makefile only. */
+#include "vim.h"
+char_u *default_vim_dir = (char_u *)"/system/usr/share/vim";
+char_u *default_vimruntime_dir = (char_u *)"";
+char_u *all_cflags = (char_u *)"arm-linux-androideabi-gcc -c -I. -Iproto -DHAVE_CONFIG_H   -I/opt/android/ev/jb/prebuilts/ndk/current/platforms/android-9/arch-arm/usr/include/  -nostdlib -D_FORTIFY_SOURCE=1      ";
+char_u *all_lflags = (char_u *)"arm-linux-androideabi-gcc   -Wl,-rpath-link=/opt/android/ev/jb/prebuilts/ndk/current/platforms/android-9/arch-arm/usr/lib/ -L/opt/android/ev/jb/prebuilts/ndk/current/platforms/android-9/arch-arm/usr/lib/ -o vim       -lm -lc -lncurses -ldl          ";
+char_u *compiled_user = (char_u *)"drew";
+char_u *compiled_sys = (char_u *)"master";
